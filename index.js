ccfbb68170a9b6b4610ea67ba3fa5a82242a13b7
Gerald Emory <geraldemory@geralds-mbp.attlocal.net>
2017-08-21 10:50:59 -0400

    made the blanks for the word

--- index.js
+++ index.js
@@ -11,11 +11,38 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(expressValidator());
+app.use(morgan('combined'));
+app.use(session({
+  secret:"mySecretKeyFromTheMovieAladin",
+  resave: false,
+  saveUninitialized: true
+}));
 const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
 
-app.get('/', (req,res)=>{
-  res.send("helloooo")
+app.use('/',(req,res,next)=>{
+  if(!req.session.word){
+  let randomWord = Math.floor(Math.random()*(words.length-1));
+  req.session.word =words[randomWord];
+  //Array(randomword.length).fill('_');
+  req.session.blank = Array(req.session.word.length).fill('_');
+  console.log(req.session.word);
+}
+next();
 });
+
+app.get('/', (req,res)=>{
+
+  res.render("index",{blank: req.session.blank.join(' ')} )
+});
+app.post('/guess',(req,res)=>{
+  let guess = req.body.guess
+  req.session.guess = guess;
+  console.log(req.session);
+  console.log(req.session.guess);
+  console.log(req.session.blank);
+  res.redirect('/');
+});
+
 
 
 app.listen(3000)
