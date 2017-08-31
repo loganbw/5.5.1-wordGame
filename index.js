9553b3bd7d6ef36fdf3a3f049bde95e81481b099
Gerald Emory <geraldemory@Geralds-MacBook-Pro.local>
2017-08-31 16:27:26 -0400

    fixed hangman to where it works

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
 const fs = require('fs');
 //----------------------------------------------------------\\
+const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
 const app = express();
+app.use('/static',express.static('public'));
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({extended: false}))
@@ -17,30 +19,55 @@ app.use(session({
   resave: false,
   saveUninitialized: true
 }));
-const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
 
-app.use('/',(req,res,next)=>{
-  if(!req.session.word){
-  let randomWord = Math.floor(Math.random()*(words.length-1));
-  req.session.word =words[randomWord];
-  //Array(randomword.length).fill('_');
-  req.session.blank = Array(req.session.word.length).fill('_');
-  console.log(req.session.word);
-}
+
+app.use('/', (req, res, next) => {
+
+
+
+if (!req.session.word){
+  let randomize = Math.floor((Math.random()*(words.length-1)));
+  let randomWord = words.filter(word => word.length > 3)[randomize].split('');
+  req.session.word = randomWord;
+  req.session.letter = Array(req.session.word.length).fill('_');
+  req.session.guess = [];
+  }
 next();
 });
 
-app.get('/', (req,res)=>{
 
-  res.render("index",{blank: req.session.blank.join(' ')} )
+
+app.get('/', (req,res)=>{
+  let hWord = req.session.word.join('');
+  let hLetter = req.session.letter.join('');
+  let guessesLeft = req.session.guess.length;
+    if(guessesLeft === 8){
+      return res.render("loss", {hWord: hWord});
+    }else if (hLetter === hWord && hWord != []){
+      return res.render("winner", {hWord: hWord});
+    };
+  res.render("index",{letterArray: req.session.letter, guesses: req.session.guess});
 });
+
+
+
 app.post('/guess',(req,res)=>{
-  let guess = req.body.guess
-  req.session.guess = guess;
-  console.log(req.session);
-  console.log(req.session.guess);
-  console.log(req.session.blank);
-  res.redirect('/');
+  if (req.session.word.includes(req.body.guessInput) == true
+    && req.session.letter.includes(req.body.guessInput) == false){
+    for (var i = 0; i < req.session.word.length; i++){
+    if (req.session.word[i] == req.body.guessInput){
+      req.session.letter.fill(req.body.guessInput, i, i + 1);
+
+        };
+      };
+      res.redirect('/');
+    }else if (req.session.letter.includes(req.body.guessInput)) {
+      return res.redirect('/');
+    }else{
+      req.session.guess.push(req.body.guessInput);
+      return res.redirect('/');
+    };
+
 });
 
 
